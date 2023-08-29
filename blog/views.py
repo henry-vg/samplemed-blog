3bce0f182a3a5d9bf472e3e2ccce8235b0de58da
henry-vg <hvg.henry@gmail.com>
2023-08-28 22:59:17 -0300

    feat: cria a view de keywords

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
-from blog.models import User
-from blog.serializers import UserSerializer
+from blog.models import User, Keyword
+from blog.serializers import UserSerializer, KeywordSerializer
+
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class KeywordView(viewsets.ModelViewSet):
+    queryset = Keyword.objects.all()
+    serializer_class = KeywordSerializer
