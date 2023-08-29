74862f9c3308add549901eeef8cc3bb00746a0a9
henry-vg <hvg.henry@gmail.com>
2023-08-29 01:21:57 +0000

    feat: cria a view de users

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from blog.models import User
+from blog.serializers import UserSerializer
 
-# Create your views here.
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
