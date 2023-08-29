470bf98e5f880a605fdeb7d4a88c0b0303fd42ce
henry-vg <hvg.henry@gmail.com>
2023-08-29 01:22:08 +0000

    feat: cria o serializer de users

---
+++ blog/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from blog.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(
+                validated_data['password'])
+        return super().update(instance, validated_data)
