4715d2ac1d875c8c6354c3b7f9785c9c06e5eaf0
henry-vg <hvg.henry@gmail.com>
2023-08-29 01:21:38 +0000

    feat: define a url de users

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from blog_app import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserView)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
