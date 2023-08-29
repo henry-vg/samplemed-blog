9ac00211b841442a18babf6f73387694e69ea5af
henry-vg <hvg.henry@gmail.com>
2023-08-29 01:04:55 +0000

    feat: cria as models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=64, unique=True)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    keyword_set = models.ManyToManyField(Keyword)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        'Comment', on_delete=models.CASCADE, null=True, blank=True)
