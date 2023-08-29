8f737f092b225540ea41dd4e6fdb62005a36b2d4
henry-vg <hvg.henry@gmail.com>
2023-08-28 22:55:43 -0300

    fix: corrige o nome do app nas urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from blog_app import views
+from blog import views
 
 router = DefaultRouter()
 router.register(r'users', views.UserView)
