a7c02213720ba7bc1cf18a6864c5a43e74ef5e7e
henry-vg <hvg.henry@gmail.com>
2023-08-29 01:54:45 -0300

    feat: cria a view de comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
-from blog.models import User, Keyword, Article
-from blog.serializers import UserSerializer, KeywordSerializer, ArticleSerializer
+from blog.models import User, Keyword, Article, Comment
+from blog.serializers import UserSerializer, KeywordSerializer, ArticleSerializer, CommentSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class UserView(viewsets.ModelViewSet):
@@ -16,3 +18,15 @@ class KeywordView(viewsets.ModelViewSet):
 class ArticleView(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        article = self.get_object()
+        comments = Comment.objects.filter(article=article)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
